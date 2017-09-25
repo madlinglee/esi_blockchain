eac7f6d8369d0aeb502e28ffcc7af59360e39d90
madlinglee <31001385+madlinglee@users.noreply.github.com>
2017-09-25 10:50:39 +0800

    添加libesipbftseal目录下文件

---
+++ libesipbftseal/pbft_client.cpp
@@ -0,0 +1,239 @@
+/**
+ * @file pbft_client.cpp
+ * @brief PBFT客户端
+ * @author LiJianfeng
+ * @date 2017年06月26日
+ */
+
+#include <iostream>
+#include "pbft_client.h"
+
+using namespace std;
+using namespace dev::eth;
+using namespace pbft;
+
+PBFTClient::PBFTClient(
+        ChainParams const& params,
+        int network_id,
+        p2p::Host* host,
+        std::shared_ptr<GasPricer> gp,
+        std::string const& db_path,
+        WithExisting we,
+        TransactionQueue::Limits const& l
+    ) : Client(params, network_id, host, gp, db_path, we, l)
+{}
+
+void PBFTClient::doWork()
+{
+    bool t = true;
+    if (m_syncBlockQueue.compare_exchange_strong(t, false))
+        syncBlockQueue();//同步本地区块，而不广播出去
+
+    if (m_needStateReset)
+    {
+        resetState();
+        m_needStateReset = false;
+    }
+
+    tick();
+
+    callQueuedFunctions();
+
+    if (!m_syncBlockQueue)
+    {
+        std::unique_lock<std::mutex> l(x_signalled);
+        m_signalled.wait_for(l, chrono::seconds(5));
+    }
+}
+
+void PBFTClient::onNewBlocks(const h256s& blocks, h256Hash& changed)
+{//取消了通知EthereumHost广播
+    for (auto const& h: blocks)
+        clog(ClientTrace) << "Live block:" << h;
+
+    for (auto const& h: blocks)
+        appendFromBlock(h, BlockPolarity::Live, changed);
+}
+
+void PBFTClient::onTransactionQueueReady()
+{//取消notify，增加通知广播交易
+    m_syncTransactionQueue = true;
+    if(auto h = m_host.lock())
+        h->noteNewTransactions();
+}
+
+void PBFTClient::syncTransactionQueue()
+{//取消通知广播交易
+    Timer timer;
+
+    h256Hash changeds;
+    TransactionReceipts new_pending_receipts;
+
+    DEV_WRITE_GUARDED(x_working)
+    {
+        if (m_working.isSealed())
+        {
+            ctrace << "Skipping txq sync for a sealed block.";
+            return;
+        }
+
+        tie(new_pending_receipts, m_syncTransactionQueue) = m_working.sync(bc(), m_tq, *m_gp);
+    }
+
+    if (new_pending_receipts.empty())
+    {
+        auto s = m_tq.status();
+        ctrace << "No transactions to process. " << m_working.pending().size() << " pending, " << s.current << " queued, " << s.future << " future, " << s.unverified << " unverified";
+        return;
+    }
+
+    DEV_READ_GUARDED(x_working)
+        DEV_WRITE_GUARDED(x_postSeal)
+            m_postSeal = m_working;
+
+    DEV_READ_GUARDED(x_postSeal)
+        for (size_t i = 0; i < new_pending_receipts.size(); i++)
+            appendFromNewPending(new_pending_receipts[i], changeds, m_postSeal.pending()[i].sha3());
+
+    // Tell farm about new transaction (i.e. restart mining).
+    onPostStateChanged();
+
+    // Tell watches about the new transactions.
+    noteChanged(changeds);
+
+    ctrace << "Processed " << new_pending_receipts.size()
+        << " transactions in" << (timer.elapsed() * 1000)
+        << "(" << (bool)m_syncTransactionQueue << ")";
+    cout << "#交易处理速度："
+        << ((new_pending_receipts.size()*1000)/((timer.elapsed()*1000000)/1000))
+        << "笔/s" << endl;
+}
+
+bool PBFTClient::sealCurrentBlockWithLock(const BlockHeader& bi)
+{
+    RLPStream s;
+    bi.streamRLP(s);
+    if(m_working.sealBlock(s.out()))
+        return true;
+    else
+        return false;
+}
+
+bytes PBFTClient::createConsensusData()
+{
+    cout << "#区块链当前高度：" << bc().number()  << endl;
+
+    if(!isMajorSyncing())
+    {
+        {
+            //UpgradableGuard l(x_working);
+            {
+                if(m_working.isSealed())
+                {
+                    //UpgradeGuard l2(l);
+                    //m_working.resetCurrent();
+                    return m_working.blockData();
+                }
+            }
+        }
+        bool t = true;
+        if(!isSyncing() && !m_remoteWorking && m_syncTransactionQueue.compare_exchange_strong(t, false))
+            syncTransactionQueue();
+        DEV_WRITE_GUARDED(x_working)
+        {
+            m_working.commitToSeal(bc(), m_extraData);
+
+            m_sealingInfo = m_working.info();
+
+            cout << "#区块当前交易个数：" << m_working.pending().size()  << endl;
+            ctrace << "Generating seal on" << m_sealingInfo.hash(WithoutSeal) << "#" << m_sealingInfo.number();
+            clog(ClientNote) << "Caching new commited block.";
+
+            bc().addBlockCache(m_working, m_working.info().difficulty());
+
+            if(sealCurrentBlockWithLock(m_sealingInfo))
+            {
+                DEV_WRITE_GUARDED(x_postSeal)
+                    m_postSeal = m_working;
+            }
+            else
+            {
+                cout << "=提交区块失败..." << endl;
+                return bytes();
+            }
+        }
+    }
+    return m_working.blockData();
+}
+
+bool PBFTClient::verify(const bytes& block)
+{
+    bc().verifyFromPBFT(block, m_stateDB);
+    return true;
+}
+
+bool PBFTClient::commit(const bytes& block)
+{
+    ImportRoute ir;
+    if(block.empty())
+    {
+        cout << "=不要将空区块入链..." << endl;
+        return false;
+    }
+    try
+    {
+        Timer t;
+        //ir = bc().import(block, m_stateDB, true);
+        ir = bc().importFromPBFT(block, m_stateDB, true);
+        double elapsed = t.elapsed();
+        clog(ClientNote) << "1 block imported in" << unsigned(elapsed * 1000)
+            << "ms (" << (1 / elapsed) << "blocks/s) in #" << bc().number();
+    }
+    catch(...)
+    {
+        cout << "=区块入链失败..." << endl;
+        resyncStateFromChain();
+        return false;
+    }
+    syncStateAfterImport(ir);
+    return true;
+}
+
+void PBFTClient::syncStateAfterImport(const ImportRoute& ir)
+{
+    if (ir.liveBlocks.empty())
+        return;
+    h256Hash changeds;
+    onDeadBlocks(ir.deadBlocks, changeds);
+    for (auto const& t: ir.goodTranactions)
+    {
+        clog(ClientTrace) << "Safely dropping transaction " << t.sha3();
+        m_tq.dropGood(t);
+    }
+    for (auto const& h: ir.liveBlocks)
+        appendFromBlock(h, BlockPolarity::Live, changeds);
+    resyncStateFromChain();
+    noteChanged(changeds);
+}
+
+void PBFTClient::broadcastPBFTMsgs(const bytes& msg)
+{
+    host()->broadcastPBFTMsgs(msg);
+}
+
+void PBFTClient::startSealing()
+{
+    bytes b = createConsensusData();
+    if(verify(b))
+        commit(b);
+}
+
+long PBFTClient::getTxNum()
+{
+    return m_tq.status().current;
+}
+
+long PBFTClient::getHeight()
+{
+    return bc().number();
+}
---
+++ libesipbftseal/pbft_client.h
@@ -0,0 +1,138 @@
+/**************************************************************************/
+/* Copyright(c) 2017-2017 Zhengzhou Esunny Information & Technology Co,.Ltd */
+/*本软件文档资料是易盛公司的资产,任何人士阅读和使用本资料必须获得相应的书面*/
+/*授权,承担保密责任和接受相应的法律约束.                                  */
+/***************************************************************************/
+/**
+ * @file pbft_client.h
+ * @brief BPFT客户端
+ * @author LiJianfeng
+ * @date 2017年06月26日
+ */
+
+#pragma once
+
+#include <libethereum/EthereumHost.h>
+#include <libethereum/Client.h>
+#include "pbft_interface.h"
+
+using namespace dev;
+using namespace dev::eth;
+
+namespace pbft{
+
+class PBFTClient : public Client, public PBFTInterface
+{
+public:
+    PBFTClient(
+        ChainParams const& params,
+        int network_id,
+        p2p::Host* host,
+        std::shared_ptr<GasPricer> gp,
+        std::string const& db_path = std::string(),
+        WithExisting we = WithExisting::Trust,
+        TransactionQueue::Limits const& l = TransactionQueue::Limits{102400, 102400}
+    );
+
+    /**
+     * @brief 创建区块并提交
+     */
+    void startSealing() override;
+
+    /**
+     * @brief 同步区块、交易等
+     */
+    void doWork() override;
+
+    /**
+     * @brief 同步区块后过滤器更新
+     *
+     * @param[in] blocks 入链区块哈希列表
+     * @param[out] changed 过滤器
+     */
+    void onNewBlocks(const h256s& blocks, h256Hash& changed) override;
+
+    /**
+     * @brief 通知广播交易
+     */
+    void onTransactionQueueReady() override;
+
+    /**
+     * @brief 同步交易队列
+     */
+    void syncTransactionQueue() override;
+
+    /**
+     * @brief 产生共识数据，即创建区块
+     *
+     * @return 区块字节数组
+     */
+    bytes createConsensusData() override;
+
+    /**
+     * @brief 验证区块
+     *
+     * @param[in] block 区块字节数组
+     *
+     * @return true 是通过 false 是失败
+     */
+    bool verify(const bytes& block) override;
+
+    /**
+     * @brief 共识提过提交，即区块入链及状态更新
+     *
+     * @param[in] block 区块字节数组
+     *
+     * @return true 是成功 false 是失败
+     */
+    bool commit(const bytes& block) override;
+
+    /**
+     * @brief 获取交易队列中当前交易数量
+     *
+     * @return 交易数量
+     */
+    long getTxNum() override;
+
+    /**
+     * @brief 获取当前区块链高度
+     *
+     * @return 区块链高度
+     */
+    long getHeight() override;
+
+    /**
+     * @brief 区块入链后的状态同步
+     *
+     * @param[in] ir 入链涉及的区块和交易
+     */
+    void syncStateAfterImport(const ImportRoute& ir);
+
+    /**
+     * @brief 广播PBFT消息给其它节点
+     *
+     * @param[in] msg 消息字节数组
+     */
+    void broadcastPBFTMsgs(const bytes& msg);
+
+    /**
+     * @brief 获取从其它节点接收的PBFT消息
+     *
+     * @return 所有消息字节数组
+     */
+    std::vector<bytes> getRecvMsgs()
+    {
+       return host()->getRecvMsgs();
+    }
+
+    EthereumHost* host()
+    {
+        if(auto h = m_host.lock())
+            return h.get();
+        BOOST_THROW_EXCEPTION(NoNetworking());
+    }
+private:
+    bool sealCurrentBlockWithLock(const BlockHeader& bi);
+};
+
+}
