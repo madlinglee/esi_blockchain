f1a706c267f2435f3ac5d56820711374d3b0d430
LiJianfeng <lijianfeng0108@qq.com>
2017-10-13 19:48:20 +0800

    扩充CLI参数

--- esi/genesis_info.h
+++ esi/genesis_info.h
@@ -19,11 +19,10 @@ using namespace dev;
 
 namespace genesis
 {
-
 const Secret secret_ljf("acf545ba38f0d5ba6851a1a209765fabc17a6c5f40e8c97e859dff7feede8bfd");
 const KeyPair keypair_ljf(secret_ljf);
 
-std::string const genesis_info_ljf =
+std::string const genesis_info =
 R"ETHEREUM(
 {
     "sealEngine": "NoProof",
@@ -32,7 +31,34 @@ R"ETHEREUM(
         "maximumExtraDataSize": "0x1000000",
         "blockReward": "0x",
         "registrar": "",
-        "networkID" : "0x45"
+        "networkID" : "0x265"
+    },
+    "genesis": {
+        "author": "0x0000000000000000000000000000000000000000",
+        "timestamp": "0x00",
+        "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "extraData": "0x",
+        "gasLimit": "0x174876E800"
+    },
+    "accounts": {
+        "0000000000000000000000000000000000000001": { "wei": "1", "precompiled": { "name": "ecrecover", "linear": { "base": 3000, "word": 0 } } },
+        "0000000000000000000000000000000000000002": { "wei": "1", "precompiled": { "name": "sha256", "linear": { "base": 60, "word": 12 } } },
+        "0000000000000000000000000000000000000003": { "wei": "1", "precompiled": { "name": "ripemd160", "linear": { "base": 600, "word": 120 } } },
+        "0000000000000000000000000000000000000004": { "wei": "1", "precompiled": { "name": "identity", "linear": { "base": 15, "word": 3 } } },
+    }
+}
+)ETHEREUM";
+
+std::string const genesis_info_ljf =
+R"E(
+{
+    "sealEngine": "NoProof",
+    "params": {
+        "accountStartNonce": "0x",
+        "maximumExtraDataSize": "0x1000000",
+        "blockReward": "0x",
+        "registrar": "",
+        "networkID" : "0x6c"
     },
     "genesis": {
         "author": "0x0000000000000000000000000000000000000000",
@@ -53,7 +79,7 @@ R"ETHEREUM(
         //"b640dd8113e51af713aed4989f3aa6afcd97c252": { "wei": "1606938044258990275541962092341162602522202993782792835301376" }
     }
 }
-)ETHEREUM";
+)E";
 
 std::string const genesis_info_basic_authority_ljf =
 R"E(
@@ -70,7 +96,7 @@ R"E(
         "durationLimit": "0x0d",
         "blockReward": "0x4563918244F40000",
         "registrar" : "0xc6d9d2cd449a754c494264e1809c50e34d64562b",
-        "networkID" : "0x1"
+        "networkID" : "0x265"
     },
     "genesis": {
         "nonce": "0x0000000000000042",
--- esi/main.cpp
+++ esi/main.cpp
@@ -7,7 +7,9 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string/trim_all.hpp>
 #include <libdevcore/FileSystem.h>
 #include <libdevcore/Common.h>
 #include <libdevcore/CommonIO.h>
@@ -62,9 +64,42 @@ private:
 
 bool ExitHandler::should_exit = false;
 
-void version()
+const string ver = "ESI BlockChain V0.1";
+
+int version()
 {
-        cout << "RPC CLI v1.0" << endl;
+    cout << EthRed << ver << " dev. via Eth." << EthReset << endl;
+    return 1;
+}
+
+int help()
+{
+    cout << EthRed
+        << "--verbosity <0-21>         设置日志等级，默认：8" << endl
+        << "--test                     开启本地测试模式" << endl
+        << "--n <整数>                 设置共识节点数量，默认：4" << endl
+        << "--c <字符串>               指定本共识节点ID" << endl
+        << "-p/--peerset <公钥@IP:端口号>" << endl
+        << "                           连接共识节点" << endl
+        << "--public-ip <IP>           设置公共网络地址，默认：自动获取" << endl
+        << "--listen-ip <IP>           监听网络连接地址，默认：0.0.0.0" << endl
+        << "--listen-port <端口号>     监听网络连接端口，默认：30303" << endl
+        << "--no-upnp                  关闭UPNP" << endl
+        << "--rpc-port <端口号>        指定RPC端口，默认：8548" << endl
+        << "--rpccorsdomain <域名>     跨域访问" << endl
+        << "-j/--json-rpc              启动RPC服务器，提供基础RPC服务" << endl
+        << "--util-rpc                 提供工具类RPC服务" << endl
+        << "--admin-rpc                提供管理员权限类RPC服务" << endl
+        << "--kill-blockchain          删除区块链数据库" << endl
+        << "--rebuild-blockchain       重构/恢复区块链数据库" << endl
+        << "--rescue-blockchain        修复区块链数据库" << endl
+        << "-d/--db-path <目录名>      指定数据库路径" << endl
+        << "--config <文件名>          读取JSON格式配置文件，包括创世块配置" << endl
+        << "--genesis-config <文件名>  读取JSON格式创世块文件" << endl
+        << "-v/--version               查看版本" << endl
+        << "-h/--help                  查看帮助" << endl
+        << EthReset;
+    return 1;
 }
 
 int main(int argc, char** argv)
@@ -73,94 +108,258 @@ int main(int argc, char** argv)
     g_logVerbosity = 8;
     
     bool test_mode = false;
-    bool util_rpc = false;
-    bool admin_rpc = false;
     
-    u256 ask_price = DefaultGasPrice;
-    u256 bid_price = DefaultGasPrice;
-    
+    unsigned int node_quantity = 4;//TODO 
+    string consensus_id = "";
+    map<NodeID, NodeIPEndpoint> nodes;
+
+    string public_ip;
     string listen_ip;
     unsigned short listen_port = 30303;
-    bool upnp = false;
+    bool upnp = true;
 
     int rpc_port = 8548;
-    int rpc_threads = 4;
     string rpc_cors_domain = "";
-    
-    string consensus_id = "";
-    string peer_ips_ports[3];
-    string peer_enodes[3];
+    bool rpc_curl = false;
+    bool rpc_util = false;
+    bool rpc_admin = false;
 
-    if(argc==2 && ((string(argv[1])=="--help") || (string(argv[1])=="--h")))
+    WithExisting we = WithExisting::Trust;
+    ChainParams cp(genesis_info);
+    string json_config;
+    string json_genesis;
+    
+    for(int i = 1; i < argc; ++i)
     {
-        cout << "--c id(必输项，e.g.71)" << endl;
-        cout << "--p ip1:port(e.g.10.10.200.71:30303) ip2:port2 ip3:port3" << endl;
-        cout << "--e enode1(e.g.413852e5c4eed59edb37b3a34cba6fc69dc3fec8b57cce49458ffc7c7de94928a53c9c60efe1b4383132ec7f7649fbb0e814a839f7516036a0dfa84b9b938d41) enode2 enode3" << endl;
-        return 1;
-    }
-    else
-    {
-        for(int i = 1; i < argc; ++i)
+        string arg = argv[i];
+        if(arg == "--verbosity" && i + 1 < argc)
         {
-            string arg = argv[i];
-            if (arg == "--c" && i + 1 < argc)
-                consensus_id = argv[++i];
-            else if(arg == "--p" && i + 1 < argc)
+            g_logVerbosity = atoi(argv[++i]);
+        }
+        else if(arg == "--test")
+        {
+            test_mode = true;
+        }
+        else if (arg == "--n" && i + 1 < argc)
+        {
+            node_quantity = atoi(argv[++i]);
+        }
+        else if (arg == "--c" && i + 1 < argc)
+        {
+            consensus_id = argv[++i];
+        }
+        else if((arg == "-p" || arg == "--peerset") && i + 1 < argc)
+        {
+            string pub;
+            string host;
+            unsigned short port = c_defaultListenPort;
+            //key@ip[:port]
+            vector<string> key_host_port;
+            boost::split(key_host_port, argv[++i], boost::is_any_of(":"));
+            if(key_host_port.size()!=2)
             {
-                peer_ips_ports[0] = argv[++i];
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
+                return -1;
             }
-            else if(arg == "--e" && i + 1 < argc)
+            port = (uint16_t)atoi(key_host_port[1].c_str());
+            vector<string> key_host;
+            boost::split(key_host, key_host_port[0], boost::is_any_of("@"));
+            if(key_host.size()!=2)
             {
-                peer_enodes[0] = argv[++i];
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
+                return -1;
             }
-            else if(arg == "--test")
+            pub = key_host[0];
+            if(pub.size() != 128)
             {
-                test_mode = true;
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
+                return -1;
             }
-            else
+            host = key_host[1];
+            Public public_key(fromHex(pub));
+            try
             {
-                cout << "搓搓搓，可以--h查询~_~" << endl;
+                nodes[public_key] = NodeIPEndpoint(bi::address::from_string(host), port, port);
+            }
+            catch(...)
+            {
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
+                return -1; 
+            }
+        }
+        else if(arg == "--public-ip" && i + 1 < argc)
+        {
+            public_ip = argv[++i];
+        }
+        else if(arg == "--listen-ip" && i + 1 < argc)
+        {
+            listen_ip = argv[++i];
+        }
+        else if(arg == "--listen-port" && i + 1 < argc)
+        {
+            listen_port = (short)atoi(argv[++i]);
+        }
+        else if(arg == "--no-upnp")
+        {
+            upnp = false;
+        }
+        else if(arg == "--rpc-port" && i + 1 < argc)
+        {
+            rpc_port = atoi(argv[++i]);
+        }
+        else if(arg == "--rpccorsdomain" && i + 1 < argc)
+        {
+            rpc_cors_domain = atoi(argv[++i]);
+        }
+        else if((arg == "-j" || arg == "--json-rpc"))
+        {
+            rpc_curl = true;
+        }
+        else if(arg == "--util-rpc")
+        {
+            rpc_util = true;
+        }
+        else if(arg == "--admin-rpc")
+        {
+            rpc_admin = true;
+        }
+        else if(arg == "--kill-blockchain")
+        {
+            we = WithExisting::Kill;
+        }
+        else if(arg == "--rebuild-blockchain")
+        {
+            we = WithExisting::Verify;
+        }
+        else if(arg == "--rescue-blockchain")
+        {
+            we = WithExisting::Rescue;
+        }
+        else if((arg == "-d" || arg == "--db-path") && i + 1 < argc)
+        {
+            setDataDir(argv[++i]);
+        }
+        else if(arg == "--config" && i + 1 < argc)
+        {
+            try
+            {
+                json_config = contentsString(argv[++i]);
+            }
+            catch(...)
+            {
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
                 return -1;
             }
         }
-        if(!test_mode)
+        else if(arg == "--genesis-config" && i + 1 < argc)
         {
-            if(consensus_id.empty())
+            try
             {
-                cout<<"请输入本节点标识(e.g.--c 71)"<<endl;
-                return -1;
+                json_genesis = contentsString(argv[++i]);
             }
-            if((peer_ips_ports[0]=="" && peer_enodes[0]!="")
-                || (peer_ips_ports[0]!="" && peer_enodes[0]==""))
+            catch(...)
             {
-                cout<<"请同时指定--p与--e"<<endl;
+                cerr << "参数[" << arg << ": " << argv[i] << "]错误" << endl;
                 return -1;
             }
         }
+        else if (arg == "-v" || arg == "--version")
+        {
+            return version();
+        }
+        else if (arg == "-h" || arg == "--help")
+        {
+            version();
+            return help();
+        }
+        else
+        {
+            cout << "搓搓搓，输入-h查询~_~" << endl;
+            return -1;
+        }
     }
+    if(!test_mode)
+    {
+        if(consensus_id.empty())
+        {
+            cout<<"请输入本节点标识(e.g.--c 71)"<<endl;
+            return -1;
+        }
+    }
+
     //向密封引擎注册商注册
     NoProof::init();
     BasicAuthority::init();
-    //构造指定json字符串的链参数
-    ChainParams cp(genesis_info_ljf);
-    //ChainParams cp(genesisInfo(eth::Network::Frontier), genesisStateRoot(eth::Network::Frontier));
+
+    //重置链操作参数
+    if(!json_config.empty())
+    {
+        try
+        {
+            cp = cp.loadConfig(json_config);
+        }
+        catch(...)
+        {
+            cerr << "指定文件的JSON格式错误" << endl;
+            cerr << "正确示例: " << endl << genesis_info_ljf << endl;
+            return -1;
+        }
+    }
+    if(!json_genesis.empty())
+    {
+        try
+        {
+            cp = cp.loadGenesis(json_genesis);
+        }
+        catch(...)
+        {
+            cerr << "指定文件的JSON格式错误" << endl;
+            string sample =
+            R"E(
+            {
+                "nonce": "0x0000000000000042",
+                "difficulty": "0x400000000",
+                "mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "author": "0x0000000000000000000000000000000000000000",
+                "timestamp": "0x00",
+                "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "extraData": "0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa",
+                "gasLimit": "0x1388"
+            }
+            )E";
+            cerr << "正确示例: " << endl << sample << endl;
+            return -1;
+        }
+    }
+
+    //允许未来区块
     cp.allowFutureBlocks = true;
+
     if(test_mode)
         cp.chainID = -1;
 
-    auto net_prefs = NetworkPreferences(listen_ip, listen_port, upnp);
+    auto net_prefs = public_ip.empty()? 
+        NetworkPreferences(public_ip, listen_ip, listen_port, upnp):
+        NetworkPreferences(listen_ip, listen_port, upnp);
     net_prefs.discovery = false;
     net_prefs.pin = true;
 
     auto hosts_state = contents(getDataDir()/fs::path( "network.rlp"));
 
     //构造Host、Client、Consenter
-    WebThreeConsensus wt(consensus_id, "ESI BLCOKCHAIN V0.1", cp, net_prefs, &hosts_state, getDataDir());
+    WebThreeConsensus wt(consensus_id, ver, cp, net_prefs, &hosts_state, getDataDir(), we);
 
+    u256 ask_price = DefaultGasPrice;
+    u256 bid_price = DefaultGasPrice;
     shared_ptr<eth::TrivialGasPricer> gp = make_shared<eth::TrivialGasPricer>(ask_price, bid_price);
     
     //创建密钥管理器
-    KeyManager km;
+    fs::path secrets_path;
+    if (test_mode)
+        secrets_path = (boost::filesystem::path(getDataDir()) / "keystore");
+    else
+        secrets_path = SecretStore::defaultPath();
+    KeyManager km(KeyManager::defaultPath(), secrets_path);
     
     auto get_password = [&](const string& prompt)
     {
@@ -182,50 +381,53 @@ int main(int argc, char** argv)
     unique_ptr<ExitHandler> eh(new ExitHandler());
 
     //配置RPC服务器端
-    unique_ptr<SessionManager> sm;
-    sm.reset(new SessionManager());
-
-    auto *hs = new SafeHttpServer(rpc_port, "", "", rpc_threads);
-    hs->setAllowedOrigin(rpc_cors_domain);
     unique_ptr<ModularServer<>> rpc_server;
-
-    EthFace* eth = new Eth(*wt.client(), *ah.get());
-    NetFace* net = new Net(wt);
-    DBFace* db = nullptr;
-    Web3Face* w3 = nullptr;
-    DebugFace* dbg = nullptr;
-    //WhisperFace* wsp = nullptr;
-    PersonalFace* per = nullptr;
-    AdminEthFace* adm_eth = nullptr;
-    AdminNetFace* adm_net = nullptr;
-    AdminUtilsFace* adm_utl = nullptr;
-
-    if(test_mode)
-        per = new Personal(km, *ah, *wt.client());
-    if(util_rpc)
+    if(rpc_curl)
     {
-        db = new LevelDB();
-        w3 = new Web3(wt.clientVersion());
-        dbg = new Debug(*wt.client());
-    }
-    if(admin_rpc)
-    {
-        adm_eth = new AdminEth(*wt.client(), *gp.get(), km, *sm.get());
-        adm_net = new AdminNet(wt, *sm.get());
-        adm_utl = new AdminUtils(*sm.get(), eh.get());
+        unique_ptr<SessionManager> sm;
+        sm.reset(new SessionManager());
+
+        auto *hs = new SafeHttpServer(rpc_port, "", "", SensibleHttpThreads);
+        hs->setAllowedOrigin(rpc_cors_domain);
+
+        EthFace* eth = new Eth(*wt.client(), *ah.get());
+        NetFace* net = new Net(wt);
+        DBFace* db = nullptr;
+        Web3Face* w3 = nullptr;
+        DebugFace* dbg = nullptr;
+        //WhisperFace* wsp = nullptr;
+        PersonalFace* per = nullptr;
+        AdminEthFace* adm_eth = nullptr;
+        AdminNetFace* adm_net = nullptr;
+        AdminUtilsFace* adm_utl = nullptr;
+
+        if(test_mode)
+            per = new Personal(km, *ah, *wt.client());
+        if(rpc_util)
+        {
+            db = new LevelDB();
+            w3 = new Web3(wt.clientVersion());
+            dbg = new Debug(*wt.client());
+        }
+        if(rpc_admin)
+        {
+            adm_eth = new AdminEth(*wt.client(), *gp.get(), km, *sm.get());
+            adm_net = new AdminNet(wt, *sm.get());
+            adm_utl = new AdminUtils(*sm.get(), eh.get());
+        }
+
+        rpc_server.reset(new ModularServer<EthFace, NetFace, DBFace, Web3Face,
+            DebugFace, PersonalFace, AdminEthFace, AdminNetFace, AdminUtilsFace>
+            (eth, net, db, w3, dbg, per, adm_eth, adm_net, adm_utl));
+        rpc_server->addConnector(hs);
+        rpc_server->StartListening();
+        
+        cout << "@RPC服务器端开启监听成功。" << endl;
+        string session_key;
+        session_key = sm->newSession(SessionPermissions{{Privilege::Admin}});
+        cout << "@会话密钥：" << session_key << endl;
     }
 
-    rpc_server.reset(new ModularServer<EthFace, NetFace, DBFace, Web3Face,
-        DebugFace, PersonalFace, AdminEthFace, AdminNetFace, AdminUtilsFace>
-        (eth, net, db, w3, dbg, per, adm_eth, adm_net, adm_utl));
-    rpc_server->addConnector(hs);
-    rpc_server->StartListening();
-    
-    cout << "@RPC服务器端开启监听成功。" << endl;
-    string session_key;
-    session_key = sm->newSession(SessionPermissions{{Privilege::Admin}});
-    cout << "@会话密钥：" << session_key << endl;
-    
     //获取PBFT客户端
     PBFTClient* pclient = static_cast<PBFTClient*>(wt.client());
     
@@ -244,23 +446,17 @@ int main(int argc, char** argv)
         //本节点的识别地址
         cout << wt.enode() << endl;
  
-        if(peer_ips_ports[0]!="")
-        {
-            for(int x=0; x<1; x++)
-            {
-                h512 e(peer_enodes[x]);
-                wt.requirePeer(e, peer_ips_ports[x]);
-            }
-        }
+        for (auto const& p: nodes)
+            wt.requirePeer(p.first, p.second);
         net_data = wt.saveNetwork();
-        if (!net_data.empty())
+        if(!net_data.empty())
             writeFile(getDataDir()/fs::path("/network.rlp"), net_data);
         
         wt.insertValidator("72");
         wt.insertValidator("71");
  
         //开启共识
-        while(wt.peerCount() < 1)//四个节点启动再开启
+        while(wt.peerCount() < node_quantity-1)//四个节点启动再开启
             ;
         cout << "@开启PBFT" <<endl;
         wt.startPBFT();
